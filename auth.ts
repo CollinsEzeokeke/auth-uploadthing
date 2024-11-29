247a1037aca23658be16e4400c4acd5ae4a145ee
Ezeokeke Collins Chidubem <collinsmecosy@gmail.com>
2024-11-29 14:05:27 +0100

    made all teh needed changes for my code and remains to correct the toast and make sure that it can function properly when needed in production

---
+++ auth.ts
@@ -0,0 +1,40 @@
+import { betterAuth, User } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { PrismaClient } from "@prisma/client";
+import { sendEmail } from '@/lib/emailVerification'
+import { passkey } from "better-auth/plugins";
+ 
+const prisma = new PrismaClient();
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "postgresql", // or "mysql", "postgresql", ...etc
+    }),
+    emailAndPassword: {    
+        enabled: true,
+        requireEmailVerification: true,
+        emailVerification: {
+            sendVerificationEmail: async ( user: User, url: string) => {
+              await sendEmail({
+                to: user.email,
+                subject: "Verify your email address",
+                text: `Click the link to verify your email: ${url}`,
+              });
+            },
+          },
+    	autoSignIn: false,
+
+    },
+    socialProviders: {
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID as string, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+        }, 
+    },
+    session: {
+        cookieCache: {
+            enabled: true,
+            maxAge: 14 * 60 // Cache duration in seconds
+        }
+    },
+    plugins: [passkey()]
+});
